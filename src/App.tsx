9f5b8a0a75f0e74effb4efe016946a438088a061
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-05-31 11:44:03 +0200

    Import Unsplash package + basic photo fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,78 @@
+import { createApi } from "unsplash-js";
+import { Fragment, useEffect, useState } from "react";
+import { ApiResponse } from "unsplash-js/dist/helpers/response";
+import { Photos } from "unsplash-js/dist/methods/search/types/response";
+
+const apiConfig = {
+  accessKey: process.env.REACT_APP_unsplash_access_key!,
+};
+const api = createApi(apiConfig);
+
+type Photo = {
+  id: number | string;
+  width: number;
+  height: number;
+  urls: { large?: string; regular: string; raw: string; small: string };
+  color: string | null;
+  user: {
+    username: string;
+    name: string;
+  };
+};
+
+const PhotoComp: React.FC<{ photo: Photo }> = ({ photo }) => {
+  const { user, urls } = photo;
+
+  return (
+    <Fragment>
+      <img className="img" src={urls.regular} />
+      <a
+        className="credit"
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        {user.name}
+      </a>
+    </Fragment>
+  );
+};
+
 const App = () => {
-  return <div className="App">Hello world :)</div>;
+  const [data, setPhotosResponse] = useState<ApiResponse<Photos>>();
+
+  useEffect(() => {
+    api.search
+      .getPhotos({ query: "work", orientation: "landscape" })
+      .then((result) => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }, []);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  } else if (data?.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="feed">
+        <ul className="columnUl">
+          {data?.response.results.map((photo) => (
+            <li key={photo.id} className="li">
+              <PhotoComp photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default App;
